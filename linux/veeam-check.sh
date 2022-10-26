2a4dccc9f4eb1caec6dbfae9f8470ac04fe14586
Steffen Müller <steffen.mueller@sovdwaer.de>
2022-10-26 09:45:38 +0200

    fix wrong parsing

--- linux/veeam-check.sh
+++ linux/veeam-check.sh
@@ -10,18 +10,39 @@
 lastSession=$(veeamconfig session list | tail -n 2 | grep -v Total)
 
 # parse line and extract expected information
-lastDate=$(echo $lastSession | awk -F' ' '{ print $7 }')
-lastTime=$(echo $lastSession | awk -F' ' '{ print $8 }')
+lastDate=$(echo $lastSession | awk -F' ' '{ print $8 }')
+lastTime=$(echo $lastSession | awk -F' ' '{ print $9 }')
 
 # check if last Backup Failed
 lastFailed=$(echo $lastSession | grep Failed)
 
 if [ -z "${lastFailed}" ]; then
-    echo "0 \"VeeamBackup\" - Last Backup State: Success, $lastDate, $lastTime"
+    echo "0 \"VeeamBackup\" - Last Backup State: Success, $lastDate $lastTime"
 else
     # parse last successful Backup line and add to output
     lastSuccessLine=$(veeamconfig session list | grep Success | tail -n 1)
     lastSuccessDate=$(echo $lastSuccessLine | awk -F' ' '{ print $7 }')
     lastSuccessTime=$(echo $lastSuccessLine | awk -F' ' '{ print $8 }')
-    echo "2 \"VeeamBackup\" - Last Backup State: Failed, $lastDate, $lastTime, Last Successful Backup: $lastSuccessDate, $lastSuccessTime"
+    echo "2 \"VeeamBackup\" - Last Backup State: Failed, $lastDate $lastTime, Last Successful Backup: $lastSuccessDate $lastSuccessTime"
 fi;
+
+
+
+#!/bin/sh
+
+lastSession=$(veeamconfig session list | tail -n 2 | grep -v Total)
+
+lastDate=$(echo $lastSession | awk -F' ' '{ print $7 }')
+lastTime=$(echo $lastSession | awk -F' ' '{ print $8 }')
+failed=$(echo $lastSession | grep Failed)
+if [ -z "${failed}" ]; then
+    echo "0 \"VeeamBackup\" - Letztes Backup: $lastDate, $lastTime"
+else
+    lastSuccessLine=$(veeamconfig session list | grep Success | tail -n 1)
+    lastSuccessDate=$(echo $lastSuccessLine | awk -F' ' '{ print $7 }')
+    lastSuccessTime=$(echo $lastSuccessLine | awk -F' ' '{ print $8 }')
+    echo "2 \"VeeamBackup\" - Letztes Backup fehlgeschlagen: $lastDate, $lastTime, Letztes erfolgreiche Backup: "
+fi;
+
+
+
